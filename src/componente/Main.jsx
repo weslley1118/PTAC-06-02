6ee64232134bf5e3a9e1b35c0bf76f00142b83e8
weslley1118 <nakamurahenrique31@gmail.com>
2024-09-01 00:19:16 -0400

    dia27/3
    dia27/3

--- src/componente/Main.jsx
+++ src/componente/Main.jsx
@@ -4,15 +4,35 @@ export default function Main() {
     const [nome, setNome] = useState(""); 
     const [telefone, setTelefone] = useState("");
 
+    const [listaContatos, setListaContatos] = useState([]);
+    const registrar = (event) => {
+        event.preventDefault();
+        setListaContatos([...listaContatos,
+            {
+                NomeSalvo: nome,
+                telefoneSalvo: telefone
+            }
+        ]);
+    };
+    console.table(listaContatos);
     return(
         <main>    
-            <h1>Nome</h1>
+            <label htmlFor="nome">Nome</label>
             <input type="nome" name="" id="" onChange={(event)=> setNome(event.target.value)}/>
             {nome}
-            <h1>Telefone</h1>
+            <label htmlFor="tel">Telefone</label>
             <input type="tel" name="" id="" onChange={(event)=> setTelefone(event.target.value)}/>
             {telefone}
-            <button>Salvar</button>
+            <button onClick={(registrar)}>Salvar</button>
+            {
+                listaContatos.map((contato, index) =>
+            <div key={index}>
+                <div className="oi">
+                <p>{contato.NomeSalvo}</p>
+                <p>{contato.telefoneSalvo}</p>
+                </div>
+                </div>
+            )}
         </main>
     );
 }
