a1d4df0c213c148cd618e9b2f3052df6390e1a1d
weslley1118 <>
2024-08-31 20:16:54 -0400

    Dia 12/3

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+
+  const nome = "João";
+  const sobreNome = "Santos";
+
+  return(
+    <footer>
+        <p>© Todos os direitos Reservados a {nome} {sobreNome}</p>
+    </footer>  
+  );
+
+
+}
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+      <header>
+        <h1>Cabeçalho</h1>
+      </header>  
+    );
+  
+  
+  }
